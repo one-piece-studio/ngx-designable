2fe494cb581f2ec76627aa0d726bff83e08f2b87
李豪珣 <519367854@qq.com>
2024-02-15 19:52:23 +0800

    feat: 添加首页

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(v => v.HomeComponent) }
+];
